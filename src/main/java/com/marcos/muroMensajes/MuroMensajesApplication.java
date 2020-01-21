93f68dcd1a88a5336dc3ca908e7e4e0d218b0620
repitaneo <fernandez.marcos@gmail.com>
2020-01-21 09:26:19 +0100

    Iniciando el proyecto

---
+++ src/main/java/com/marcos/muroMensajes/MuroMensajesApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.muroMensajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuroMensajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuroMensajesApplication.class, args);
+	}
+
+}
