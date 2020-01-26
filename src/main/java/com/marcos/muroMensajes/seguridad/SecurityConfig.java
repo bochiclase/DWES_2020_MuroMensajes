8449ea409f309fdb1e6f937157fe9c1288cfb982
repitaneo <fernandez.marcos@gmail.com>
2020-01-26 19:44:12 +0100

    Seguridad funcionando

---
+++ src/main/java/com/marcos/muroMensajes/seguridad/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.marcos.muroMensajes.seguridad;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.marcos.muroMensajes.servicios.Autenticacion;
+
+@Configuration
+@EnableWebSecurity(debug=true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	
+    @Autowired
+    private Autenticacion autenticacion;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	http
+        .authorizeRequests()
+	        .anyRequest().authenticated()
+	        .and()    	
+        .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .defaultSuccessUrl("/")
+            .failureUrl("/login?error=true")
+            .usernameParameter("username")
+            .passwordParameter("password")
+            .and()
+            .csrf().disable()
+        .logout()
+            .permitAll()
+            .logoutSuccessUrl("/login?logout");
+    }
+    
+    
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        
+    	DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(autenticacion);
+        return provider;
+    }
+
+}
