4d9d3cecfb61e2c4875d6c7e1206d8bfd13410ae
repitaneo <fernandez.marcos@gmail.com>
2020-02-19 11:16:59 +0100

    Consultas con SQL

--- src/main/java/com/marcos/muroMensajes/datos/usuarios/UsuarioDAO.java
+++ src/main/java/com/marcos/muroMensajes/datos/usuarios/UsuarioDAO.java
@@ -1,6 +1,7 @@
 package com.marcos.muroMensajes.datos.usuarios;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +13,26 @@ import org.springframework.data.repository.query.Param;
 @Repository
 public interface UsuarioDAO extends CrudRepository<Usuario,String> {
 
+
+	@Transactional @Query(value="Update usuario u set usuario='?2'WHERE usuario='?1'",nativeQuery=true)
+	void actualizarNombreUsuario(String userViejo,String userNuevo);
+
+	
+	
+	@Transactional @Query(value="Select * from usuarios where edad=:edad",nativeQuery=true)
+	void buscarPorEdad(@Param("edad") Integer anios);
+	
+	
+	
+	@Query("from usuarios where edad=:edad")
+	void buscarPorEdad2(@Param("edad") Integer anios);
+	
+	
+	
+	
+	
+	
+	
 	List<Usuario> findByEdad(Integer edad);
 	List<Usuario> findByEdadLessThan(Integer edad);	
 	List<Usuario> findByEdadLessThanEqual(Integer edad);	
